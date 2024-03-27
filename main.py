4c69847e5c3e16ce04f46475cf1df536f5138d90
Ivan Terentyev <tvvtvy.terentjev@yandex.ru>
2024-03-28 00:39:32 +0300

    add logs

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import json
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 from environs import Env
+import logging
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+# Конфигурируем логирование
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
 
 # Создаем экземпляр класса Env для загрузки переменных окружения
 env = Env()
@@ -13,9 +23,6 @@ transport = RequestsHTTPTransport(
     headers={'apollographql-client-name': env('GRAPH_REF')},  # Заголовок с именем клиента
     use_json=True,
 )
-
-# Создаем клиент GraphQL
-client = Client(transport=transport, fetch_schema_from_transport=False)
 
 # GraphQL-запрос
 query = gql('''
@@ -49,23 +56,33 @@ query Query {
 }
 ''')
 
-# Выполнение GraphQL-запроса
-result = client.execute(query)
+def main(transport, query):
 
-# Создание словаря для последующего сохранения в формате JSON
-res = {'JSON': []}
+    # Создаем клиент GraphQL
+    client = Client(transport=transport, fetch_schema_from_transport=False)
 
-# Обход JSON
-for product in result['search']['products']['products']:
-    # Добавление информации о продукте в словарь res (id, name, url, regular_price, promo_price, brand)
-    res['JSON'].append({'id': product['id'],
-                        'name': product['name'],
-                        'url': f'online.metro-cc.ru{product['url']}',
-                        'regular_price': product['stocks'][0]['prices']['old_price'] if product['stocks'][0]['prices']['old_price'] else product['stocks'][0]['prices']['price'],
-                        'promo_price': product['stocks'][0]['prices']['price'] if product['stocks'][0]['prices']['old_price'] else None,
-                        'brand': product['manufacturer']['name']})
+    # Выполнение GraphQL-запроса
+    result = client.execute(query)
 
-# Сохранение результата в файл JSON
-with open('products.json', 'w') as file:
-    json.dump(res, file, indent=2)
+    # Создание словаря для последующего сохранения в формате JSON
+    res = {'JSON': []}
 
+    # Обход JSON
+    for product in result['search']['products']['products']:
+        # Добавление информации о продукте в словарь res (id, name, url, regular_price, promo_price, brand)
+        res['JSON'].append({'id': product['id'],
+                            'name': product['name'],
+                            'url': f'online.metro-cc.ru{product['url']}',
+                            'regular_price': product['stocks'][0]['prices']['old_price'] if
+                            product['stocks'][0]['prices']['old_price'] else product['stocks'][0]['prices']['price'],
+                            'promo_price': product['stocks'][0]['prices']['price'] if product['stocks'][0]['prices'][
+                                'old_price'] else None,
+                            'brand': product['manufacturer']['name']})
+
+    # Сохранение результата в файл JSON
+    with open('products.json', 'w') as file:
+        json.dump(res, file, indent=2)
+
+if __name__ == '__main__':
+    logger.info('Starting parser')
+    main(transport, query)
